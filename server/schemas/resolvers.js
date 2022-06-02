117a94bb2f93e39a11df67939a7f87de63f4171b
Shawn Willard <shawnmwillard@gmail.com>
2022-06-01 20:14:50 -0500

    auth.js middleware function updated to work with GraphQL API

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,80 +27,12 @@ const resolvers = {
       return { token, user };
     },
 
-    const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
-
-const resolvers = {
-  Query: {
-    me: async (parent, args, context) => {
-        if (context.user) {
-          const userData = await User.findOne({ _id: context.user._id })
-            .select('-__v -password');
-          return userData;
-        }
-  
-        throw new AuthenticationError('Not logged in');
-      },
-  },
-
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args)
- 
-      if (!user) {
-        return 'Something is wrong!';
-      }
-      const token = signToken(user);
-
-      return { token, user };
-    },
-
     loginUser: async (parent, args) => {
       const user = await User.findOne({ $or: [{ username: args.username }, { email: args.email }] });
 
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
-      const correctPw = await user.isCorrectPassword(args.password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-      return { token, user };
-    },
-    saveBook: async (parent, args, context) => {
-      if (context.user) {
-        return await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { savedBooks: args} },
-          { new: true, runValidators: true }
-        );
-      }
-
-      throw new AuthenticationError('You need to be logged in!!');
-    },
-
-    removeBook: async (parent, args, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId: args.bookId } } },
-          { new: true }
-        );
-
-        return updatedUser;
-      }
-
-      throw new AuthenticationError('You need to be logged in!');
-    }
-  }
-};
-
-module.exports = resolvers;
 
       const correctPw = await user.isCorrectPassword(args.password);
 
