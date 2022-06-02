5e350478b4ac3078f877fb5c5071cf806f5b4d3b
Shawn Willard <shawnmwillard@gmail.com>
2022-06-01 20:02:38 -0500

    Query & Mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,36 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
-
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
+  type Query {
+    me: User
+  }
+  type Mutation {
+    loginUser(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, title:String!, bookId: String!, image: String, link: String): User
+    removeBook(bookId: String!): User
+  }
+`;
 // export the typeDefs
 module.exports = typeDefs;
